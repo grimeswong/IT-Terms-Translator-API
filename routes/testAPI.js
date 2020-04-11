457f0a90545e50fcee411f650d01979dc1d56936
Shadow Grimes <grimeswongatwork@gmail.com>
2020-04-11 23:53:24 +1000

    Refine the response of results without any object name on top of it

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
         if(err) throw err;
         connection.release();
         console.log('Query executing...')
-        res.json({results});
+        res.json(results);
       });
     })
 });
