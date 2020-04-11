6390130d01a91cfc23bf8b66fda0f1c327d0332a
Shadow Grimes <grimeswongatwork@gmail.com>
2020-04-12 07:51:04 +1000

    Increase the query connections limit

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,7 +3,7 @@ var router = express.Router();
 require('dotenv').config();
 var mysql      = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
+  connectionLimit : 1000,
   host            : process.env.DB_HOST,
   user            : process.env.DB_USER,
   password        : process.env.DB_PW,
